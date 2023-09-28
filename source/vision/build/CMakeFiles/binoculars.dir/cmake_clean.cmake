605100df7698c56d48b69687ff9f47f72b31d0aa
ULis3h <ulis2020@163.com>
2023-09-28 11:02:31 +0800

    add some

---
+++ source/vision/build/CMakeFiles/binoculars.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/binoculars.dir/src/armor.cpp.o"
+  "CMakeFiles/binoculars.dir/src/util/rbtree.cpp.o"
+  "libbinoculars.a"
+  "libbinoculars.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/binoculars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/vision/build/test/util/rbtree/CMakeFiles/test_rbtree.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ulis/Documents/Code/hoaRadish/source/vision/test/util/rbtree/main.cpp" "/Users/ulis/Documents/Code/hoaRadish/source/vision/build/test/util/rbtree/CMakeFiles/test_rbtree.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  ".././include"
+  "../../../include/common"
+  "../test/util/rbtree/."
+  "../test/util/rbtree/../../../include/comm"
+  "../test/util/rbtree/../../../include/util"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/ulis/Documents/Code/hoaRadish/source/vision/build/CMakeFiles/binoculars.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
