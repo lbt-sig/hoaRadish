587bd94798020ef86d860c52324f77041bf93d9d
ULis <ulis2020@163.com>
2023-10-18 19:57:10 +0800

    add FileTranser

---
+++ tools/LBTFileTranser/receiver.cpp
@@ -0,0 +1,132 @@
+﻿// MIT License
+
+// Copyright (c) 2023 LBT_sig
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#define CLOSE_SOCKET(s) closesocket(s)
+#else
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define CLOSE_SOCKET(s) close(s)
+#endif
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+        return 1;
+    }
+
+    const int port = std::stoi(argv[1]);
+
+#ifdef _WIN32
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Failed to initialize Winsock." << std::endl;
+        return 1;
+    }
+#endif
+
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == -1) {
+        std::cerr << "Failed to create socket." << std::endl;
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(serverSocket, reinterpret_cast<struct sockaddr*>(&serverAddress), sizeof(serverAddress)) == -1) {
+        std::cerr << "Failed to bind to the port." << std::endl;
+        CLOSE_SOCKET(serverSocket);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    if (listen(serverSocket, 5) == -1) {
+        std::cerr << "Failed to listen on the port." << std::endl;
+        CLOSE_SOCKET(serverSocket);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    std::cout << "Waiting for incoming connections..." << std::endl;
+
+    sockaddr_in clientAddress;
+    int clientSocket;
+    int clientAddressLength = sizeof(clientAddress);
+
+    clientSocket = accept(serverSocket, reinterpret_cast<struct sockaddr*>(&clientAddress), &clientAddressLength);
+    if (clientSocket == -1) {
+        std::cerr << "Failed to accept the connection." << std::endl;
+        CLOSE_SOCKET(serverSocket);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    char filename[1024];
+    recv(clientSocket, filename, sizeof(filename), 0);
+
+    std::ofstream fileStream(filename, std::ios::binary);
+    if (!fileStream.is_open()) {
+        std::cerr << "Failed to create the file." << std::endl;
+        CLOSE_SOCKET(clientSocket);
+        CLOSE_SOCKET(serverSocket);
+#ifdef _WIN32
+        WSACleanup();
+#endif
+        return 1;
+    }
+
+    char buffer[1024];
+    int bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0) > 0)) {
+        fileStream.write(buffer, bytesRead);
+    }
+
+    std::cout << "File received successfully." << std::endl;
+
+    CLOSE_SOCKET(clientSocket);
+    CLOSE_SOCKET(serverSocket);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+
+    return 0;
+}
